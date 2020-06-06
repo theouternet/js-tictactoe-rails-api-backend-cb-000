70307b16a7661f69ab6b6ac27b87aafbd5a6746e
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-06 06:55:47 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,30 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  
+  
+  def index 
+    @games = Game.all 
+    render json: @games 
+  end 
 
+
+  def create 
+    @game = Game.create(game_params) 
+    render json: @game, status: 201
+  end 
+
+  def update 
+    @game = Game.find(params[:id])
+    @game.update(game_params)
+    render json: @game 
+  end 
+
+  def show 
+    @game = Game.find(params[:id])
+    # respond_to do |format|
+    #   format.json {render json: @game.to_json(only: [:id, :state])}
+    # end 
+    render json: @game
+    # binding.pry
+  end 
 end
