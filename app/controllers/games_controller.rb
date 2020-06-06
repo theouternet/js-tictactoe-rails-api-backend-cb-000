b5ca46b6d3fa6229e37204b1b524d59d9cec6984
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-06 08:27:58 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,40 +1,22 @@
-class GamesController < ApplicationController
-  # Add your GamesController code here
-     before_action :set_game, only: [:show, :update] 
-  
-  def index 
-    @games = Game.all 
-    render json: @games 
-  end 
-
-
-  def create 
-    @game = Game.create(game_params) 
+ def create
+    @game = Game.create(state: ["X", "", "", "", "", "", "", "", ""])
     render json: @game, status: 201
-  end 
+  end
 
-  def update 
+
+  def show
     @game = Game.find(params[:id])
-    @game.update(game_params)
-    render json: @game 
-  end 
+    render json: @game, status: 201
+  end
 
-  def show 
-    @game = Game.find(params[:id])
-    # respond_to do |format|
-    #   format.json {render json: @game.to_json(only: [:id, :state])}
-    # end 
-    render json: @game
-    # binding.pry
-  end 
+  def index
+    @games = Game.all
+    render json: @games, status: 201
+  end
 
-  private 
-
-  def game_params 
-    params.permit(state: [])
-  end 
-
-  def set_game 
-    game = Game.find(params[:id])
-  end 
+  def update
+    @game = Game.update(state: params[:state])
+    render json: @game, status: 201
+  end
+end
 end
